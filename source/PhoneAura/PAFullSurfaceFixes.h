3d103fcf1de13ac241ecc7d66ed8cea1c56179c8
zeshan0727 <Zeshanbarvi@gmail.com>
2026-07-20 20:20:10 +0300

    Export full-surface functions to Logos C++

--- source/PhoneAura/PAFullSurfaceFixes.h
+++ source/PhoneAura/PAFullSurfaceFixes.h
@@ -1,5 +1,13 @@
 #import <UIKit/UIKit.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void PAApplyFullSurfaceFixes(UIViewController *controller);
 void PARefreshFullSurfaceForTabController(UITabBarController *tabController);
 void PARestoreFullSurfaceFixes(UIViewController *controller);
+
+#ifdef __cplusplus
+}
+#endif
