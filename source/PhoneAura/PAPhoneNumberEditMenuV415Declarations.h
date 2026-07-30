df4be785a142511739f009d480d42b2c97965c0b
zeshan0727 <Zeshanbarvi@gmail.com>
2026-07-30 13:19:27 +0300

    Declare PhoneAura number editing categories

---
+++ source/PhoneAura/PAPhoneNumberEditMenuV415Declarations.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+@class PhoneAuraManager;
+@class PAStudioKeypadView;
+
+@interface PhoneAuraManager (PAKeypadPasteBridgeV415)
+- (void)pasteNumberIntoKeypad:(NSString *)number;
+@end
+
+@interface PAStudioKeypadView (PAKeypadPasteBridgeV415)
+- (void)setDialNumber:(NSString *)number;
+@end
